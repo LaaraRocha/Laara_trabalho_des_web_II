5e6c951e429a96870d2a384e4f3ca77e477b8748
1018528 <laara.rocha@gmail.com>
2022-03-03 19:44:27 -0300

    Commit 2

---
+++ Classes/A_class.php
@@ -0,0 +1,18 @@
+<?php
+
+class a
+{
+    private $url;
+    private $texto;
+
+    function __construct($pUrl, $pTexto)
+    {
+        $this->url = $pUrl;
+        $this->texto = $pTexto;
+    }
+
+    function __toString()
+    {
+        return "<a href=\"{$this->url}\">{$this->texto}</a>";
+    }
+}
